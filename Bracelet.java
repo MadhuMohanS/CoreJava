746d409e46fd7e0e91b05b801e6dcc3ce91c905a
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-02 11:46:07 +0530

    Chaining task

---
+++ Bracelet.java
@@ -0,0 +1,36 @@
+class Bracelet{
+    double cost;
+	String color;
+	String shape;
+	String symbol;
+	String type;
+	
+	Bracelet(){
+		super();
+	}
+	Bracelet(double cost){
+		this.cost=cost;
+		System.out.println("Bracelet cost:"+cost);
+	}
+	Bracelet(double cost,String color){
+		this(cost);
+		this.color=color;
+		System.out.println("Bracelet cost:"+cost+" color:"+color);
+	}
+	Bracelet(double cost,String color,String shape){
+		this(cost,color);
+		this.shape=shape;
+		System.out.println("Bracelet cost:"+cost+" color:"+color+" shape:"+shape);
+	}
+	Bracelet(double cost,String color,String shape,String symbol){
+		this(cost,color,shape);
+		this.symbol=symbol;
+		System.out.println("Bracelet cost:"+cost+" color:"+color+" shape:"+shape+" symbol:"+symbol);
+	}
+	
+	Bracelet(double cost,String color,String shape,String symbol,String type){
+		this(cost,color,shape,symbol);
+		this.type=type;
+		System.out.println("Bracelet cost:"+cost+" color:"+color+" shape:"+shape+" symbol:"+symbol+" type:"+type);
+	}
+}
