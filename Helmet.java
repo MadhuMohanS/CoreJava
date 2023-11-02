561e1d4a71a01504bdee9236a34d67ddeff531d3
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-02 11:45:04 +0530

    Chaining task

---
+++ Helmet.java
@@ -0,0 +1,30 @@
+class Helmet{
+	String brand;
+	String color;
+	String type;
+	String typeOfVechile;
+	double cost;
+	
+	Helmet(){
+		super();
+	}
+	Helmet(String brand){
+		System.out.println("Helmet brand:"+brand);
+	}
+	Helmet(String brand,String color){
+		this(brand);
+		System.out.println("Helmet brand:"+brand+" color:"+color);
+	}
+	Helmet(String brand,String color,String type){
+		this(brand,color);
+		System.out.println("Helmet brand:"+brand+" color:"+color+" type:"+type);
+	}
+	Helmet(String brand,String color,String type,String typeOfVechile){
+		this(brand,color,type);
+		System.out.println("Helmet brand:"+brand+" color:"+color+" type:"+type+" type of vehicle:"+typeOfVechile);
+	}
+	Helmet(String brand,String color,String type,String typeOfVechile,double cost){
+		this(brand,color,type,typeOfVechile);
+		System.out.println("Helmet brand:"+brand+" color:"+color+" type:"+type+" type of vehicle:"+typeOfVechile+" cost:"+cost);
+	}
+}
