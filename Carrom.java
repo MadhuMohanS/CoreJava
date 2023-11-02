a3edd3ba89916cbc24f19ab80bd0487aafb30d5a
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-02 11:46:30 +0530

    Chaining task

---
+++ Carrom.java
@@ -0,0 +1,39 @@
+class Carrom{
+	String game;
+	double cost;
+	String company;
+	String type;
+	String typeOfCoins;
+	
+	Carrom(){
+		super();
+	}
+	
+	Carrom(String game){
+		this.game=game;
+		System.out.println("carrom game is:"+game);
+	}
+	Carrom(String game,double cost){
+		this(game);
+		this.cost=cost;
+		System.out.println("carrom game is:"+game+" cost:"+cost);
+	}
+	Carrom(String game,double cost,String company){
+		this(game,cost);
+		this.company=company;
+		System.out.println("carrom game is:"+game+" cost:"+cost+" company:"+company);
+	}
+	
+	Carrom(String game,double cost,String company,String type){
+		this(game,cost,company);
+		this.type=type;
+		System.out.println("carrom game is:"+game+" cost:"+cost+" company:"+company+" type:"+type);
+	}
+	
+	Carrom(String game,double cost,String company,String type,String typeOfCoins){
+		this(game,cost,company,type);
+		this.typeOfCoins=typeOfCoins;
+		System.out.println("carrom game is:"+game+" cost:"+cost+" company:"+company+" type:"+type+" type of coins:"+typeOfCoins);
+	}
+	
+}
