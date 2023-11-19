df464033e48b18c1073357ea092382b3d646ba19
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-19 23:43:45 +0530

    type casting

---
+++ Generator.java
@@ -0,0 +1,9 @@
+class Generator extends Machine{
+	Generator(){
+		System.out.println("running no-arg constructor of generator");
+	}
+	
+	void switchOn(){
+		System.out.println("running switchon in generator");
+	}
+}
