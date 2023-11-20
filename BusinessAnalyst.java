3994d757f62abe3ca01d2b6d063d2a687571416e
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:49:25 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ BusinessAnalyst.java
@@ -0,0 +1,9 @@
+class BusinessAnalyst extends Person1{
+	BusinessAnalyst(){
+		System.out.println("creating no-arg constructor of BusinessAnalyst");
+	}
+	
+	void checkRequirements(){
+		System.out.println("running checkRequirements in BusinessAnalyst");
+	}
+}
