7b7db7cb714e5d402d425418e1aaa5ceb809d5e3
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:50:20 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ DataBaseDeveloper.java
@@ -0,0 +1,9 @@
+class DataBaseDeveloper extends Developer{
+	DataBaseDeveloper(){
+		System.out.println("creating no-arg constructor of ProductOwner");
+	}
+	
+	void runScripts(){
+		System.out.println("running runScripts in DataBaseDeveloper");
+	}
+}
