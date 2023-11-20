b8f05de977381b56785f28e46c1fff283dcdabec
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:47:37 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ Person1.java
@@ -0,0 +1,13 @@
+class Person1{
+	Person1(){
+		System.out.println("creating no-arg constructor of person");
+	}
+	
+	void eat(){
+		System.out.println("running eat in person");
+	}
+	
+	void sleep(){
+		System.out.println("running sleep in person");
+	}
+}
