66d74a65ff77261a81da9adf5f8f962d538f5cf5
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:48:58 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ Tester.java
@@ -0,0 +1,9 @@
+class Tester extends Developer{
+	Tester(){
+		System.out.println("creating no-arg constructor of tester");
+	}
+	
+	void qualityCheck(){
+		System.out.println("running qualityCheck in tester");
+	}
+}
