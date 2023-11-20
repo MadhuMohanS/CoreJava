0befc4c25ac9db03a507305c9bd0d2b76558e83d
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:49:55 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ ProductOwner.java
@@ -0,0 +1,9 @@
+class ProductOwner extends Person1{
+	ProductOwner(){
+		System.out.println("creating no-arg constructor of ProductOwner");
+	}
+	
+	void validate(){
+		System.out.println("running validate in ProductOwner");
+	}
+}
