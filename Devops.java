7e20193cd7f76c3b98e8bfad70c4bec57cc350a9
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-20 18:50:39 +0530

    passing only the parent reference and accessing all child class methods using typecaste and instanceOf keyword

---
+++ Devops.java
@@ -0,0 +1,9 @@
+class Devops extends Person1{
+	Devops(){
+		System.out.println("creating no-arg constructor of Devops");
+	}
+	
+	void runBuild(){
+		System.out.println("running runbuild in devops");
+	}
+}
