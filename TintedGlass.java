bc2f17e6b22a7e1cee7596b3f08a05dc7428495d
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-21 18:01:15 +0530

    initialzing instance variables and inheriting using super()

---
+++ TintedGlass.java
@@ -0,0 +1,5 @@
+class TintedGlass extends ToughenedGlass{
+	TintedGlass(double weight,String ownerName,double length,int thickness,GlassType type,String usage,String brand){
+	super(weight,ownerName,length,thickness,type,usage,brand);
+	}
+}
