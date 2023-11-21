714c88f3cbab1b0f645091d728314a9e135372d9
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-21 18:01:41 +0530

    initialzing instance variables and inheriting using super()

---
+++ GorillaGlass.java
@@ -0,0 +1,5 @@
+class GorillaGlass extends LaminatedGlass{
+	GorillaGlass(double cost,int warranty,double length,int thickness,GlassType type,String usage,String brand){
+		super(cost,warranty,length,thickness,type,usage,brand);
+	}
+}
