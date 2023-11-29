3097f4e63cc75e0792b2c3a46a74bf6c303bdb21
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 23:00:03 +0530

    toString() and hashCode()

---
+++ Geyser.java
@@ -0,0 +1,15 @@
+package com.stringpackage.objectintro;
+
+public class Geyser {
+double cost=2000;
+String company="Bosch";
+int capacity=20;
+
+
+@Override
+public String toString() {
+	return "Geyser [cost=" + cost + ", company=" + company + ", capacity=" + capacity + "]";
+}
+
+
+}
