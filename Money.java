04ee088c0715aeedebce630bb6fb9ee4d44fb41b
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 23:00:23 +0530

    toString() and hashCode()

---
+++ Money.java
@@ -0,0 +1,15 @@
+package com.stringpackage.objectintro;
+
+public class Money {
+
+	int note=200;
+	String color="pink";
+	int noOfCount=100;
+
+
+
+	@Override
+	public String toString() {
+		return "Money [note=" + note + ", color=" + color + ", noOfCount=" + noOfCount + "]";
+	}
+}
