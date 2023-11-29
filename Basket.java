f709e421c97991654e4d64f95008ef868c217c51
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:58:23 +0530

    toString() and hashCode()

---
+++ Basket.java
@@ -0,0 +1,18 @@
+package com.stringpackage.objectintro;
+
+import java.util.Objects;
+
+public class Basket {
+	double cost=300;
+	String color="blue";
+	int size=30;
+	
+	@Override
+	public String toString() {
+		return "basket info[cost:300,color:blue,size:30]";
+	}
+
+
+	
+	
+}
