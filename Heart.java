65fecd68ffc65fd96a3429bbdf642ae40694971f
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:56:33 +0530

    toString() and hashCode()

---
+++ Heart.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class Heart {
+String gender="male";
+int age=30;
+String disease="heartattack";
+
+@Override
+public String toString() {
+	return "heart info[gender:male,age:30,disease:heartattack]";
+}
+}
