b90b167ebdcf6495a838f0f8fae58a6da06314da
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:58:38 +0530

    toString() and hashCode()

---
+++ bowl.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class bowl {
+double cost=20;
+int size=20;
+String type="glass";
+
+@Override
+public String toString() {
+	return "bowl info[cost:20,size:20,type:glass]";
+}
+}
