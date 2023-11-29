125c1ccb428271b6a28d68d418ee75df144aaf13
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:56:17 +0530

    toString() and hashCode()

---
+++ Mic.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class Mic {
+int cost=1000;
+String company="canon";
+String audio="good";
+
+@Override
+public String toString() {
+	return "Mic info[cost:1000,comapny:canon,audio:good]";
+}
+}
