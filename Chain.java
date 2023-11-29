7208b0d9ccacd1efd0b5c54778018ae7ed31371a
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:57:05 +0530

    toString() and hashCode()

---
+++ Chain.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class Chain {
+int cost=200;
+String vehicle="bike";
+int length=10;
+
+@Override
+public String toString() {
+	return "Chain info[cost:200,vehicle:bike,length:10]";
+}
+}
