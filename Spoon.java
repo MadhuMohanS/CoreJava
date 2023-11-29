c724da0a0cc7382363d052ffdb2326de43a7c607
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:59:02 +0530

    toString() and hashCode()

---
+++ Spoon.java
@@ -0,0 +1,11 @@
+package com.stringpackage.objectintro;
+
+public class Spoon {
+int cost=20;
+String type="silver";
+int count=2;
+
+public String toString() {
+	return "spoon info[cost:20,type:silver,count=2]";
+}
+}
