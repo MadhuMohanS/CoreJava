2d7862e83ef0229fe5d080bb5aa259594f90149f
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:57:31 +0530

    toString() and hashCode()

---
+++ Rice.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class Rice {
+int quantity=100;
+double cost=50;
+String comapny="Jeera";
+
+@Override
+public String toString() {
+  return "rice info[quantity;100,cost:50,comapny:jeera]";
+}
+}
