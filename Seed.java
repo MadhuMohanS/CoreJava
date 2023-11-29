ba9e6295dbf6b573625eadcd8c8fd29aaa5b2328
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:56:49 +0530

    toString() and hashCode()

---
+++ Seed.java
@@ -0,0 +1,13 @@
+package com.stringpackage.objectintro;
+
+public class Seed {
+
+	int cost=200;
+	String plant="mango";
+	int count=3;
+	
+	@Override
+	public String toString() {
+		return "seed info:[cost:200,plant:mango,count:3]";
+	}
+}
