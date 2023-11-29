49e060cf3860c22c2cd5e9ced9c72bfa8b2e571a
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-11-29 22:58:04 +0530

    toString() and hashCode()

---
+++ Bucket.java
@@ -0,0 +1,12 @@
+package com.stringpackage.objectintro;
+
+public class Bucket {
+double cost=300;
+String color="blue";
+int size=30;
+
+@Override
+public String toString() {
+	return "bucket info[cost:300,color:blue,size:30]";
+}
+}
