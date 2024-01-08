bb3940780a425ec6ff81af6c4a15b74c1cfde250
MadhuMohanS <madhumohan.xworkz@gmail.com>
2024-01-08 22:49:15 +0530

    few methods of Collection methods

---
+++ collectionprograms/MovieCollection.java
@@ -0,0 +1,20 @@
+package com.xworkz.collection.collectionprograms;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MovieCollection {
+
+	public Collection<String> movieCollection(){
+		Collection<String> movieCollection=new ArrayList<>();
+		movieCollection.add("12th fail");
+		movieCollection.add("salaar");
+		movieCollection.add("chinna");
+		movieCollection.add("ragada");
+		movieCollection.add("sambahudur");
+		movieCollection.add("guntur karam");
+		movieCollection.add("jabili");
+		return movieCollection;
+	}
+
+}
